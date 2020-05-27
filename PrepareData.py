d12812a4bca8126652e21b8a63babeb98e299db6
Pin Quan <tanpinquan@gmail.com>
2020-05-27 21:19:19 +0800

    Unwrap data

--- PrepareData.py
+++ PrepareData.py
@@ -1,13 +1,31 @@
 import turicreate as tc
 from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
 
 data_dir = './Exercise Data/'
+
 
 def find_label_for_containing_interval(intervals, index):
     containing_interval = intervals[:, 0][(intervals[:, 1] <= index) & (index <= intervals[:, 2])]
     if len(containing_interval) == 1:
         # print(containing_interval[0])
         return containing_interval[0]
+
+
+def unwrap_data(sf):
+    col_names = np.array(sf.column_names())
+    col_ind = np.array(range(col_names.__len__()))
+    unwrap_ind = col_ind[np.floor(col_ind/3) % 2 != 0]
+    unwrap_col = list(col_names[unwrap_ind])
+    # print(unwrap_col)
+    # print(unwrap_col.__len__())
+    for col_name in unwrap_col:
+        # plt.plot(sf[str(col_name)])
+        sf[str(col_name)] = np.unwrap(sf[str(col_name)])
+        # plt.plot(sf[str(col_name)])
+        plt.show()
+
 
 # Load labels
 labels = tc.SFrame.read_csv(data_dir + 'labels.csv', delimiter=',', header=True,
@@ -17,19 +35,23 @@ labels = tc.SFrame.read_csv(data_dir + 'labels.csv', delimiter=',', header=True,
 # labels
 print(labels)
 
-data_files = glob(data_dir + 'data*.csv')
+data_files = glob(data_dir + 'shoulder*.csv')
 
 # Load data
 data = tc.SFrame()
 files = sorted(data_files)
 for data_file in files:
-    exp_id = int(data_file[-5])
+    exp_id = int(data_file[-7])
     print('--------------', exp_id, '----------')
 
     # Load accel data
-    sf = tc.SFrame.read_csv(data_file, delimiter=',', header=True, verbose=False)
+    sf = tc.SFrame.read_csv(data_file, delimiter=',', header=True, verbose=False, column_type_hints=float)
+    col_names = sf.column_names()
+    sf = sf.remove_columns(col_names[-6:])
     # sf = sf.rename({'X1': 'acc_x', 'X2': 'acc_y', 'X3': 'acc_z'})
     sf['exp_id'] = exp_id
+
+    unwrap_data(sf)
 
     # Calc labels
     exp_labels = labels[labels['exp_id'] == exp_id][['activity_id', 'start', 'end']].to_numpy()
@@ -39,10 +61,11 @@ for data_file in files:
 
     data = data.append(sf)
 
+print(sf.shape)
 
 target_map = {
-    1.: 'knee',
-    2.: 'shoulder',
+    1.: 'shoulder_left',
+    2.: 'shoulder_right',
 
 }
 
--- TrainModel.py
+++ TrainModel.py
@@ -6,11 +6,11 @@ data = tc.SFrame('exercise_data.sframe')
 # Train/test split by recording sessions
 train, test = tc.activity_classifier.util.random_split_by_session(data,
                                                                   session_id='exp_id',
-                                                                  fraction=0.9)
+                                                                  fraction=0.8)
 
 # Create an activity classifier
 model = tc.activity_classifier.create(train, session_id='exp_id', target='activity',
-                                      prediction_window=50)
+                                      prediction_window=350,max_iterations=10)
 
 # Evaluate the model and save the results into a dictionary
 metrics = model.evaluate(test)
@@ -20,4 +20,4 @@ print(metrics['accuracy'])
 model.save('hapt.model')
 
 # Export for use in Core ML
-model.export_coreml('MyCustomActivityClassifier.mlmodel')
+model.export_coreml('ShoulderExerciseClassifier.mlmodel')
